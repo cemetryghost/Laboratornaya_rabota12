a280393245aeda8e1b8a75e5ae67c3f17c6d20da
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2022-04-22 23:36:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String reg = "\\b((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){3}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\b"; 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter IP address: ");
+        String ip = in.nextLine();
+        Pattern pat = Pattern.compile(reg);
+        Matcher match = pat.matcher(ip);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("ip.txt"))){
+            if(match.find()){
+                bw.write("Correct IP address: " + match.group());
+                bw.flush();
+            }
+            else{
+                bw.write("Correct IP address not found");
+                bw.flush();
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
